26275963534a7732f65ee7da499439050024aa31
Filippo-Timo <filtimo2002@gmail.com>
2026-03-11 16:48:51 +0100

    Adds AuthContext.jsx and modifies App.jsx and main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>HomePage</h1>} />
+        <Route path="/auth" element={<h1>AuthPage</h1>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
